35dcd7d0ed2c8e7703bf5c103895c8f56c1de0a0
Vedant Mathur <vedant@mathur.ca>
2016-02-14 02:46:12 -0800

    Fixed the typos
    Seriously Kellen?

--- app/src/main/java/com/example/willy/simcal/Calendar.java
+++ app/src/main/java/com/example/willy/simcal/Calendar.java
@@ -24,7 +24,7 @@ public class Calendar extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_calandar, menu);
+        getMenuInflater().inflate(R.menu.menu_calendar, menu);
         return true;
     }
 
